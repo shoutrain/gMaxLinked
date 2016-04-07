53ba9a5062d1e636af51d45b62d43ce4226eae2f
rafael <shoutrain.goo@gmail.com>
2016-04-07 16:40:15 +0800

    initial the project

---
+++ src/gLink.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : gLink.cpp
+ Author      : Rafael Gu
+ Version     :
+ Copyright   : GPL
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
